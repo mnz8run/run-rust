54ee57b49596be63317738a8d25c66b3e5ebf1c5
mnz8 <mnz8gg@gmail.com>
2023-01-02 17:26:18 +0800

    2.3 神秘数字

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::io;
+use rand::Rng;
+use std::io; // trait
 
 fn main() {
+    println!("猜数游戏");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("神秘数字 {}", secret_number);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("读取错误");
     println!("你猜的数字是 {}", guess);
