a741d09cd3c82b0396bed7dc0a212a1facae55ae
mnz8 <mnz8gg@gmail.com>
2023-01-02 17:45:48 +0800

    2.4 多次猜测

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@ use std::cmp::Ordering;
 use std::io; // trait
 
 fn main() {
-    println!("猜数游戏，猜测一个数字");
+    println!("猜数游戏");
     let secret_number = rand::thread_rng().gen_range(1..101); // i32 u32 i64
-    println!("神秘数字 {}", secret_number);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("读取错误");
+    loop {
+        println!("猜测一个数字");
 
-    // shadow
-    let guess: u32 = guess.trim().parse().expect("not a number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取错误");
 
-    println!("你猜的数字是 {}", guess);
+        // shadow
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        println!("你猜的数字是 {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
